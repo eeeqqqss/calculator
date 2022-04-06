b73bf69f16b23408c993950a2ae896aac697be06
Гость <Guest@iMac-Mac005-9.local>
2022-04-06 17:36:07 +0900

    first commit

---
+++ 666/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  666
+//
+//  Created by Гость on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var buttonZero: UIButton!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var buttonChangeResult: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
